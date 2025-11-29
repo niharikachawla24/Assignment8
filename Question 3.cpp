57e1431e622eef089d69cb47f9e8b8d20b24f3a3
niharikachawla24 <nchawla_be24@thapar.edu>
2025-11-29 07:54:49 +0530

    Update Question 3.cpp

--- Question 3.cpp
+++ Question 3.cpp
@@ -1,116 +1,119 @@
 //Question 3 (a)
 #include <iostream>
-struct Node{
-    int data;
-    Node* left;
-    Node* right;
-};  
+struct Tree {
+    int val;
+    Tree* lchild;
+    Tree* rchild;
+};
 
-Node* add_node(Node* parent,int data){
-  Node* NewNode= new Node;
-    
-    if(parent==NULL){
-        NewNode->data=data;
-        NewNode->left=NULL;
-        NewNode->right=NULL;
-          return NewNode;
+Tree* insertNode(Tree* root, int val) {
+    Tree* t = new Tree;
+
+    if (root == NULL) {
+        t->val = val;
+        t->lchild = NULL;
+        t->rchild = NULL;
+        return t;
     }
-    
-    if(data<parent->data)
-    {  parent->left=add_node(parent->left,data);}
-    if(data>parent->data)
-     {  parent->right=add_node(parent->right,data);}
-    return parent;
 
+    if (val < root->val)
+        root->lchild = insertNode(root->lchild, val);
+    else if (val > root->val)
+        root->rchild = insertNode(root->rchild, val);
+
+    return root;
 }
 
-
 int main() {
-   Node* Node=NULL;
-Node= add_node(Node,5);
-Node= add_node(Node,6);
-Node= add_node(Node,3);
+    Tree* head = NULL;
+    head = insertNode(head, 15);
+    head = insertNode(head, 7);
+    head = insertNode(head, 25);
 
-    std::cout<<"Root:"<<Node->data<<std::endl;
-    std::cout<<"Left:"<<Node->left->data<<std::endl;
-    std::cout<<"Right:"<<Node->right->data<<std::endl;
+    std::cout << "Root : " << head->val << std::endl;
+    std::cout << "Left : " << head->lchild->val << std::endl;
+    std::cout << "Right: " << head->rchild->val << std::endl;
+
     return 0;
 }
 
 //Question 3 (b)
 #include <iostream>
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
+struct Tree {
+    int info;
+    Tree* left;
+    Tree* right;
 };
 
-Node* add_node(Node* parent, int data) {
-    if (parent == NULL) {
-        Node* NewNode = new Node;
-        NewNode->data = data;
-        NewNode->left = NULL;
-        NewNode->right = NULL;
-        return NewNode;
+Tree* add(Tree* root, int info) {
+    if (root == NULL) {
+        Tree* t = new Tree;
+        t->info = info;
+        t->left = NULL;
+        t->right = NULL;
+        return t;
     }
-    if (data < parent->data) parent->left = add_node(parent->left, data);
-    else if (data > parent->data) parent->right = add_node(parent->right, data);
-    return parent;
+
+    if (info < root->info) root->left = add(root->left, info);
+    else if (info > root->info) root->right = add(root->right, info);
+
+    return root;
 }
 
-Node* find_min(Node* node) {
-    if (node == NULL || node->left == NULL) return node;
-    return find_min(node->left);
+Tree* smallest(Tree* curr) {
+    if (curr == NULL || curr->left == NULL) return curr;
+    return smallest(curr->left);
 }
 
-Node* delete_node(Node* root, int key) {
+Tree* removeNode(Tree* root, int key) {
     if (root == NULL) return root;
 
-    if (key < root->data)
-        root->left = delete_node(root->left, key);
-    else if (key > root->data)
-        root->right = delete_node(root->right, key);
+    if (key < root->info)
+        root->left = removeNode(root->left, key);
+    else if (key > root->info)
+        root->right = removeNode(root->right, key);
     else {
         if (root->left == NULL) {
-            Node* temp = root->right;
+            Tree* tmp = root->right;
             delete root;
-            return temp;
+            return tmp;
         }
         else if (root->right == NULL) {
-            Node* temp = root->left;
+            Tree* tmp = root->left;
             delete root;
-            return temp;
+            return tmp;
         }
-        Node* temp = find_min(root->right);
-        root->data = temp->data;
-        root->right = delete_node(root->right, temp->data);
+
+        Tree* tmp = smallest(root->right);
+        root->info = tmp->info;
+        root->right = removeNode(root->right, tmp->info);
     }
     return root;
 }
 
-void inorder(Node* root) {
+void inorder(Tree* root) {
     if (root == NULL) return;
     inorder(root->left);
-    std::cout << root->data << " ";
+    std::cout << root->info << " ";
     inorder(root->right);
 }
 
 int main() {
-    Node* root = NULL;
-    root = add_node(root, 20);
-    root = add_node(root, 8);
-    root = add_node(root, 22);
-    root = add_node(root, 4);
-    root = add_node(root, 12);
-    root = add_node(root, 10);
-    root = add_node(root, 14);
+    Tree* root = NULL;
+    root = add(root, 50);
+    root = add(root, 30);
+    root = add(root, 70);
+    root = add(root, 20);
+    root = add(root, 40);
+    root = add(root, 60);
+    root = add(root, 80);
 
     std::cout << "In-order before deletion: ";
     inorder(root);
     std::cout << "\n";
 
-    int key = 8;
-    root = delete_node(root, key);
+    int key = 30;
+    root = removeNode(root, key);
 
     std::cout << "In-order after deleting " << key << ": ";
     inorder(root);
@@ -121,102 +124,95 @@ int main() {
 
 //Question 3 (c)
 #include <iostream>
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
+struct Tree {
+    int val;
+    Tree* left;
+    Tree* right;
 };
 
-Node* add_node(Node* parent, int data) {
-    if (parent == NULL) {
-        Node* NewNode = new Node;
-        NewNode->data = data;
-        NewNode->left = NULL;
-        NewNode->right = NULL;
-        return NewNode;
+Tree* insert(Tree* root, int val) {
+    if (root == NULL) {
+        Tree* t = new Tree;
+        t->val = val;
+        t->left = NULL;
+        t->right = NULL;
+        return t;
     }
-    if (data < parent->data) parent->left = add_node(parent->left, data);
-    else if (data > parent->data) parent->right = add_node(parent->right, data);
-    return parent;
+    if (val < root->val) root->left = insert(root->left, val);
+    else if (val > root->val) root->right = insert(root->right, val);
+
+    return root;
 }
 
-int max_depth(Node* root) {
+int height(Tree* root) {
     if (root == NULL) return -1;
-    int left_depth = max_depth(root->left);
-    int right_depth = max_depth(root->right);
-    int depth;
-    if (left_depth > right_depth)
-        depth = left_depth + 1;
-    else
-        depth = right_depth + 1;
-    return depth;
+
+    int h1 = height(root->left);
+    int h2 = height(root->right);
+
+    return (h1 > h2 ? h1 : h2) + 1;
 }
 
 int main() {
-    Node* root = NULL;
-    root = add_node(root, 20);
-    root = add_node(root, 8);
-    root = add_node(root, 22);
-    root = add_node(root, 4);
-    root = add_node(root, 12);
-    root = add_node(root, 10);
-    root = add_node(root, 14);
+    Tree* root = NULL;
+    root = insert(root, 40);
+    root = insert(root, 20);
+    root = insert(root, 60);
+    root = insert(root, 10);
+    root = insert(root, 30);
+    root = insert(root, 50);
+    root = insert(root, 70);
 
-    std::cout << "Maximum Depth (in edges): " << max_depth(root) << "\n";
+    std::cout << "Maximum Depth: " << height(root) << "\n";
+
     return 0;
 }
 
 //Question 3 (d)
 #include <iostream>
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
+struct Tree {
+    int val;
+    Tree* left;
+    Tree* right;
 };
 
-Node* add_node(Node* parent, int data) {
-    if (parent == NULL) {
-        Node* NewNode = new Node;
-        NewNode->data = data;
-        NewNode->left = NULL;
-        NewNode->right = NULL;
-        return NewNode;
+Tree* addNode(Tree* root, int val) {
+    if (root == NULL) {
+        Tree* t = new Tree;
+        t->val = val;
+        t->left = NULL;
+        t->right = NULL;
+        return t;
     }
-    if (data < parent->data) parent->left = add_node(parent->left, data);
-    else if (data > parent->data) parent->right = add_node(parent->right, data);
-    return parent;
+
+    if (val < root->val) root->left = addNode(root->left, val);
+    else if (val > root->val) root->right = addNode(root->right, val);
+
+    return root;
 }
 
-int min_depth(Node* root) {
+int minDepth(Tree* root) {
     if (root == NULL) return -1;
     if (root->left == NULL && root->right == NULL) return 0;
 
-    int left_depth = min_depth(root->left);
-    int right_depth = min_depth(root->right);
-    int depth;
+    int leftd = minDepth(root->left);
+    int rightd = minDepth(root->right);
 
-    if (root->left == NULL)
-        depth = right_depth + 1;
-    else if (root->right == NULL)
-        depth = left_depth + 1;
-    else if (left_depth < right_depth)
-        depth = left_depth + 1;
-    else
-        depth = right_depth + 1;
+    if (root->left == NULL) return rightd + 1;
+    if (root->right == NULL) return leftd + 1;
 
-    return depth;
+    return (leftd < rightd ? leftd : rightd) + 1;
 }
 
 int main() {
-    Node* root = NULL;
-    root = add_node(root, 20);
-    root = add_node(root, 8);
-    root = add_node(root, 22);
-    root = add_node(root, 4);
-    root = add_node(root, 12);
-    root = add_node(root, 10);
-    root = add_node(root, 14);
+    Tree* root = NULL;
+    root = addNode(root, 100);
+    root = addNode(root, 50);
+    root = addNode(root, 150);
+    root = addNode(root, 25);
+    root = addNode(root, 75);
 
-    std::cout << "Minimum Depth (in edges): " << min_depth(root) << "\n";
+    std::cout << "Minimum Depth: " << minDepth(root) << "\n";
+
     return 0;
 }
